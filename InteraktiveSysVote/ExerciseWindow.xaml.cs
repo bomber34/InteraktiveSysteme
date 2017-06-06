4adbb9db4700b86e5b4a4db5a292b33533ce8222
bomber34 <kunstkr1tik@web.de>
2017-06-06 15:25:33 +0200

    Added missing files

---
+++ InteraktiveSysVote/ExerciseWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InteraktiveSysVote
+{
+    /// <summary>
+    /// Interaktionslogik für ExerciseWindow.xaml
+    /// </summary>
+    public partial class ExerciseWindow : UserControl
+    {
+        private string subjectName;
+        private int goalPresent, numberOfTasks, numberOfAssignements;
+        private SubjectPanel parentField;
+        private GeneralSubjectOverview generalOverview;
+
+        public ExerciseWindow()
+        {
+            InitializeComponent();
+        }
+
+        public ExerciseWindow(SubjectPanel parent, string name, int minPresent, int numTasks, int numExercises)
+        {
+            InitializeComponent();
+
+            //set class attributes
+            subjectName = name;
+            goalPresent = minPresent;
+            numberOfTasks = numTasks;
+            numberOfAssignements = numExercises;
+
+            parentField = parent;
+
+            SubjectName.Content = subjectName;
+            generalOverview = new GeneralSubjectOverview()
+            {
+                Height = 175
+            };
+            generalOverview.GoalPresent.Content = minPresent.ToString();
+            ExerciseStack.Children.Add(generalOverview);
+        }
+
+        private int AverageVoted()
+        {
+            int totalDone = 0, totalAll = 0;
+            foreach(ExercisePanel exer in ExerciseStack.Children.OfType<ExercisePanel>())
+            {
+                totalDone += exer.DoneTasks;
+                totalAll += exer.TotalTasks;
+            }
+
+            //Prevent dividing by 0 later on
+            if (totalDone == 0 && totalAll == 0)
+                return 0;
+
+            double average = (double)totalDone / (double) totalAll;
+            average = Math.Round(average*100);
+            
+            return (int) average;
+        }
+
+        private void DeleteExerciseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ExerciseStack.Children.Count > 1)
+                ExerciseStack.Children.RemoveAt(ExerciseStack.Children.Count - 1);
+        }
+
+        private void HomeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            parentField.avgVoted.Content = AverageVoted().ToString();
+            parentField.presented.Content = generalOverview.NumPresentations.Content;
+            HomeWindow.ReturnToMainMenu();
+        }
+
+        private void AddExerciseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ExercisePanel exercise = new ExercisePanel(numberOfTasks);
+            ExerciseStack.Children.Add(exercise);
+        }
+    }
+}
---
+++ InteraktiveSysVote/GeneralSubjectOverview.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InteraktiveSysVote
+{
+    /// <summary>
+    /// Interaktionslogik für GeneralSubjectOverview.xaml
+    /// </summary>
+    public partial class GeneralSubjectOverview : UserControl
+    {
+        public GeneralSubjectOverview()
+        {
+            InitializeComponent();
+            // TODO add average voting in % and maybe how many tasks are expected
+        }
+
+        private void PresentUp_Click(object sender, RoutedEventArgs e)
+        {
+            //NumPresentation is a label which is initialized with a digit, therefore parsing is safe
+            int num = Int32.Parse(NumPresentations.Content.ToString());
+            num++;
+            NumPresentations.Content = num.ToString();
+        }
+
+        private void PresentDown_Click(object sender, RoutedEventArgs e)
+        {
+            //NumPresentation is a label which is initialized with a digit, therefore parsing is safe
+            int num = Int32.Parse(NumPresentations.Content.ToString());
+            if (num > 0) // There can't be negative presentations
+                num--;
+            NumPresentations.Content = num.ToString();
+        }
+    }
+}
